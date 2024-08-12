c54c4ded3700cae8a78d643ff10a0454125568e6
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-08-11 21:01:26 -0700

    init phrases

---
+++ backend/src/phrases.js
@@ -0,0 +1,17 @@
+const phrases = [
+  "It's Donny's fault",
+  "Juatin has it handled",
+  "Just check the google sheet",
+  "Yes, definitely.",
+  "It is certain.",
+  "Without a doubt.",
+  "Yes, in due time.",
+  "My sources say no.",
+  "Not today.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Outlook not so good.",
+  "Very doubtful.",
+];
+
+module.exports = phrases;
